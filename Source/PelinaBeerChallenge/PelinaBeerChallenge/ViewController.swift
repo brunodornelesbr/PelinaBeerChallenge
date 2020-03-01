0e66f4f965dbad099c4a631da1bd49d39668f46e
Bruno Dorneles <>
2020-03-01 15:44:02 -0300

    :tada: initial commit

---
+++ Source/PelinaBeerChallenge/PelinaBeerChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PelinaBeerChallenge
+//
+//  Created by Bruno Dorneles on 01/03/20.
+//  Copyright © 2020 Bruno Dorneles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
