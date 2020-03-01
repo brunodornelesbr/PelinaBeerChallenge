0323a335459fe7914ca9d5d7ba864e2c61043fcb
Bruno Dorneles <>
2020-03-01 17:19:49 -0300

    :iphone: Created Best Movies Screen

---
+++ Source/PelinaBeerChallenge/PelinaBeerChallenge/BestMovies/BestMoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BestMoviesViewController.swift
+//  PelinaBeerChallenge
+//
+//  Created by Bruno Dorneles on 01/03/20.
+//  Copyright © 2020 Bruno Dorneles. All rights reserved.
+//
+
+import UIKit
+
+class BestMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
